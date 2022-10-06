655d59fa30af3cc2174d80a063e661ef7e278155
John D. Martin III <john.d.martin.iii@unc.edu>
2022-10-06 08:43:50 -0400

    Update random_number.js

--- express/lib/random_number.js
+++ express/lib/random_number.js
@@ -1,6 +1,8 @@
 // Code goes here
 // Function that returns a random number from 0-10000
 
-const makeRandomNumber = () => {
+const make_random = () => {
   Math.floor(Math.random() * 10);
 }
+
+export default make_random;
