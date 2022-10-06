0474e2380fc2f438246693b28f16720184672b2c
Caleb Gill <calebbg@UNC-R90YDFXE.localdomain>
2022-10-06 08:50:01 -0400

    Created pos_or_neg.js

---
+++ pos_or_neg.js
@@ -0,0 +1,12 @@
+function positiveOrNegative(number) {
+    if (number > 0)
+    {
+        console.log("Your number is positive");
+    }
+    else if (number == 0) {
+        console.log("Your number is zero");
+    }
+    else {
+        console.log("Your number is negative");
+    }
+}
